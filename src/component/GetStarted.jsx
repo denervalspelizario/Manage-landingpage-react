5dde8331a5ccf7a0f93d3d15900f665face0744c
denervalspelizario <denervalspelizario@gmail.com>
2022-12-15 19:01:59 -0300

    update app manage criacao jsx component header

---
+++ src/component/GetStarted.jsx
@@ -0,0 +1,11 @@
+import '../style/component/GetStarted.sass';
+
+const GetStarted = () => {
+  return (
+    <div>
+        <button type='submit'>Iniciar</button>
+    </div>
+  )
+}
+
+export default GetStarted
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,8 +1,34 @@
 import '../style/component/Header.sass';
+import GetStarted from './GetStarted';
+import Logo from '/image/logo.svg';
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <img src={Logo} alt="logo manage escrito na cor azul marinho em fonte Be Vietnam Pro" />  
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Preços</a>
+          </li>
+          <li>
+            <a href="#">Produtos</a>
+          </li>
+          <li>
+            <a href="#">Sobre nós</a>
+          </li>
+          <li>
+            <a href="#">Carreiras</a>
+          </li>
+          <li>
+            <a href="#">Comunidade</a>
+          </li>
+        </ul>
+      </nav> 
+      <GetStarted />      
+    </header>
+
   )
 }
 
