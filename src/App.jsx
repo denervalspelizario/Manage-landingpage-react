1dd9d74dcafebe7c537473817eeb3c41e6a8ef3b
denervalspelizario <denervalspelizario@gmail.com>
2022-12-23 18:44:40 -0300

    update app manage  insercao de jsx de menu mobile ja com logica

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import About from './component/About'
 import Carrers from './component/Carrers'
 import Community from './component/Community'
@@ -6,11 +7,15 @@ import Header from './component/Header'
 import './style/App.sass'
 
 function App() {
-  
+  const [ openMode, setOpenMode ] = useState(false);
+
+    const alterMenu = () => {
+        setOpenMode(!openMode)
+    }  
 
   return (
     <div className="App">
-     <Header />
+     <Header alterMenu={alterMenu} openMode={openMode} />
      <About />
      <Community />
      <Carrers />
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,7 +4,8 @@ import Banner from './Banner';
 import Logo from '/image/logo.svg';
 
 
-const Header = () => {
+const Header = (props) => {
+  
   return (
     <div>
       <header>
@@ -28,6 +29,22 @@ const Header = () => {
             </li>
           </ul>
         </nav> 
+        <nav className="container__menu--mobile">
+          <button id="hamburger" className={props.openMode ? 'bottom__open': 'bottom__close'}  onClick={props.alterMenu}>
+            <i className="menuIcon material-icons " >menu</i>
+            <i className="closeIcon material-icons">close</i>
+          </button> 
+          <menu id="menu__responsive--links" className={props.openMode ? 'menu__open': 'menu__close'}>
+            <ul>
+              <li><a className="menuLogo" href="#"><img  className="header--logo--menu" src={Logo} alt="Logo loopStudio" title="loopStudio"/></a></li>
+              <li><a href="#">Pricing</a></li>
+              <li><a href="#">Product</a></li>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Careers</a></li>
+              <li><a href="#">Community</a></li>
+            </ul> 
+          </menu>
+        </nav>
         <GetStarted />      
       </header>
       <Banner />
